5e665a52d59cf464933d18109a60f24c3eb41ee1
David Adams <david.riding.rails@gmail.com>
2021-02-16 15:41:09 -0700

    update ArticleController

--- src/main/java/com/tts/myBlog/controller/ArticleController.java
+++ src/main/java/com/tts/myBlog/controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.tts.myBlog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.tts.myBlog.model.Article;
+
 @Controller
 public class ArticleController {
 
     @GetMapping(value = "/")
-    public String index() {
+    public String index(Article article) {
       return "article/index";
     }
 }
