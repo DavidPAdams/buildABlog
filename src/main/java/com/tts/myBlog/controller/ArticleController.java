9552e1b6b4a7e4c58fec3b23fe5cf4a7a233ed55
David Adams <david.riding.rails@gmail.com>
2021-02-16 15:47:22 -0700

    add create method to ArticleController

--- src/main/java/com/tts/myBlog/controller/ArticleController.java
+++ src/main/java/com/tts/myBlog/controller/ArticleController.java
@@ -1,15 +1,29 @@
 package com.tts.myBlog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tts.myBlog.model.Article;
+import com.tts.myBlog.repository.ArticleRepository;
 
 @Controller
 public class ArticleController {
+  
+  @Autowired
+  private ArticleRepository articleRepository;
 
-    @GetMapping(value = "/")
-    public String index(Article article) {
-      return "article/index";
-    }
+  @GetMapping(value = "/")
+  public String index(Article article) {
+    return "article/index";
+  }
+  
+  @PostMapping(value = "/")
+  public String create(Article article, Model model) {
+    articleRepository.save(article);
+    model.addAttribute("article", article);
+    return "article/show";
+  }
 }
