592995f8a14ea3af432a61992e6b39c9d7f07122
David Adams <david.riding.rails@gmail.com>
2021-02-17 09:03:54 -0700

    add updateArticle() to ArticleController

--- src/main/java/com/tts/myBlog/controller/ArticleController.java
+++ src/main/java/com/tts/myBlog/controller/ArticleController.java
@@ -14,30 +14,30 @@ import com.tts.myBlog.repository.ArticleRepository;
 
 @Controller
 public class ArticleController {
-  
+
   @Autowired
   private ArticleRepository articleRepository;
 
-  @GetMapping(value = {"/", "/articles"})
+  @GetMapping(value = { "/", "/articles" })
   public String index(Article article, Model model) {
     Iterable<Article> articles = articleRepository.findAll();
     model.addAttribute("articles", articles);
     return "article/index";
   }
-  
+
   @GetMapping(value = "/articles/new")
   public String getNewArticleForm(Model model) {
     model.addAttribute("article", new Article());
     return "article/new";
   }
-  
+
   @PostMapping(value = "/articles/new")
   public String create(Article article, Model model) {
     articleRepository.save(article);
     model.addAttribute("article", article);
     return "article/show";
   }
-  
+
   @GetMapping(value = "/articles/{articleId}")
   public String showById(@PathVariable("articleId") Long articleId, Model model) {
     Optional<Article> optionArticle = articleRepository.findById(articleId);
@@ -45,13 +45,13 @@ public class ArticleController {
     model.addAttribute("article", articleFound);
     return "article/show";
   }
-  
+
   @GetMapping(value = "/articles/{articleId}/delete")
   public String deleteArticle(@PathVariable("articleId") Long articleId) {
     articleRepository.deleteById(articleId);
     return "redirect:/articles";
   }
-  
+
   @GetMapping(value = "/articles/{articleId}/edit")
   public String getEditForm(@PathVariable("articleId") Long articleId, Model model) {
     Optional<Article> optionArticle = articleRepository.findById(articleId);
@@ -59,7 +59,17 @@ public class ArticleController {
     model.addAttribute("article", articleFound);
     return "article/edit";
   }
-  
-  
-  
+
+  @PostMapping(value = "/articles/{articleId}/edit")
+  public String updateArticle(@PathVariable("articleId") Long articleId, Article article, Model model) {
+    Optional<Article> optionArticle = articleRepository.findById(articleId);
+    Article updateArticle = optionArticle.get();
+    updateArticle.setTitle(article.getTitle());
+    updateArticle.setAuthor(article.getAuthor());
+    updateArticle.setEntry(article.getEntry());
+    articleRepository.save(updateArticle);
+    model.addAttribute("article", updateArticle);
+    return "article/show";
+  }
+
 }
