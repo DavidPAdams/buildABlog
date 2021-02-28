0ce5c837ccce97fd8cb6006f90e5a1e6bb4e7c95
David Adams <david.riding.rails@gmail.com>
2021-02-28 12:14:44 -0700

    update ArticleRepository with method signature

--- src/main/java/com/tts/myBlog/repository/ArticleRepository.java
+++ src/main/java/com/tts/myBlog/repository/ArticleRepository.java
@@ -1,5 +1,7 @@
 package com.tts.myBlog.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.tts.myBlog.model.Article;
 
 @Repository
 public interface ArticleRepository extends CrudRepository<Article, Long> {
-
+  public List<Article> findAllByOrderByCreatedAtDesc();
 }
